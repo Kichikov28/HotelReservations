fa6b69fcaff8eea27dfda8d5523c913ac2b0ecde
Kichikov28 <dkichikov4@gmail.com>
2024-03-31 14:26:37 +0300

    Startup commit

---
+++ HotelReservations/HotelReservations.Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservations.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservations.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+
+}
