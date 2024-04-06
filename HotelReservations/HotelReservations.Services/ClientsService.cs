dc5c3b9a59d03a59deeb8112d61baa28b92eb3d6
Ati <adjivgova1331@abv.bg>
2024-04-06 18:14:33 +0300

    Change layout

--- HotelReservations/HotelReservations.Services/ClientsService.cs
+++ HotelReservations/HotelReservations.Services/ClientsService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelReservations.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HotelReservations.Services
 {
-    public class ClientsService
+    public class ClientsService:IClientsService
     {
     }
 }
--- HotelReservations/HotelReservations.Services/ReservationsService.cs
+++ HotelReservations/HotelReservations.Services/ReservationsService.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelReservations.Services.Contracts;
 
 namespace HotelReservations.Services
 {
-    public class ReservationsService
+    public class ReservationsService:IReservationsService
     {
     }
 }
