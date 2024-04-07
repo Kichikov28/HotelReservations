70976095f60a267c953190d5f1502125ca34d9f1
Ati <adjivgova1331@abv.bg>
2024-04-07 22:25:39 +0300

    Working....

--- HotelReservations/HotelReservations.Services/RoomsService.cs
+++ HotelReservations/HotelReservations.Services/RoomsService.cs
@@ -1,5 +1,6 @@
 ﻿using HotelReservations.Data;
 using HotelReservations.Data.Models;
+using HotelReservations.Data.Models.Enums;
 using HotelReservations.Services.Contracts;
 using HotelReservations.ViewModels.Rooms;
 using System;
@@ -33,5 +34,27 @@ namespace HotelReservations.Services
             await context.SaveChangesAsync();
             return room.Id;
         }
+        //public async Task<IndexRoomsViewModel> GetRoomsAsync(IndexRoomsViewModel model)
+        //{
+        //    model.Rooms = await this.context.Rooms
+        //        .Where(x => x.Capacity >= model.Filter.Capacity)
+        //        .Where(x => model.Filter.Type != null ? x.RoomType == Enum.Parse<RoomType>(model.Filter.Type) : x.Id != null)
+        //        .Where(x => model.Filter.IsAvailable != null ? x.IsAvailable == Convert.ToBoolean(model.Filter.IsAvailable) : x.Id != null)
+        //        .Skip((model.Page - 1) * model.ItemsPerPage)
+        //        .Take(model.ItemsPerPage)
+        //        .Select(x => new RoomIndexViewModel()
+        //        {
+        //            Id = x.Id,
+        //            Capacity = x.Capacity,
+        //            RoomType = x.RoomType,
+        //            IsAvailable = x.IsAvailable,
+        //            PricePerBedAdult = x.PricePerBedAdult,
+        //            PricePerBedChild = x.PricePerBedChild,
+        //            Number = x.Number,
+        //        })
+        //        .ToListAsync();
+        //    model.ElementsCount = await this.context.Customers.CountAsync();
+        //    return model;
+        //}
     }
 }
