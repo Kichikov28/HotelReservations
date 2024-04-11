0b1efe3ec2657b75fc21119e086600181f6057e3
Ati <adjivgova1331@abv.bg>
2024-04-11 11:49:28 +0300

    add one more viewModel...

---
+++ HotelReservations/HotelReservations.ViewModels/Reservations/IndexReservationsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using HotelReservations.ViewModels.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservations.ViewModels.Reservations
+{
+    public class IndexReservationsViewModel : PagingViewModel
+    {
+        public IndexReservationsViewModel(int elementsCount, int itemsPerPage = 5, string action = "Index") : base(elementsCount, itemsPerPage, action)
+        {
+        }
+
+        public ICollection<IndexReservationViewModel> Reservations { get; set; }
+    }
+}
