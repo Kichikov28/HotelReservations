9a06e4857cb0b223c3e2b7d6c224fd26eeb86a67
Ati <adjivgova1331@abv.bg>
2024-04-11 11:02:46 +0300

    Add create and index viewModels in Reservations...

---
+++ HotelReservations/HotelReservations.ViewModels/Reservations/CreateReservationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using HotelReservations.Data.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservations.ViewModels.Reservations
+{
+    public class CreateReservationViewModel
+    {
+        public string UserId { get; set; }
+        public ICollection<Client> Customers { get; set; } =
+            new List<Client>();
+        [Required(ErrorMessage = "Please select and submit a room")]
+        public string RoomId { get; set; }
+        public SelectList Rooms { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayName("Accomodation date")]
+        public DateTime AccommodationDate { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayName("Leave date")]
+        public DateTime LeaveDate { get; set; }
+
+        [DisplayName("Breakfast")]
+        public bool HasBreakfast { get; set; }
+
+        [DisplayName("Allinclusive")]
+        public bool HasAllInclusive { get; set; }
+    }
+}
---
+++ HotelReservations/HotelReservations.ViewModels/Reservations/IndexReservationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using HotelReservations.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservations.ViewModels.Reservations
+{
+    public class IndexReservationViewModel
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public Room Room { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        [DisplayName("Accomodation date")]
+        public DateTime AccommodationDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        [DisplayName("Leave date")]
+        public DateTime LeaveDate { get; set; }
+
+        [DisplayName("Breakfast")]
+        public bool HasBreakfast { get; set; }
+
+        [DisplayName("Allinclusive")]
+        public bool HasAllInclusive { get; set; }
+
+        [Display(Name = "Total price")]
+        public decimal Price { get; set; }
+    }
+}
